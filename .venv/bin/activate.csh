0bc04a6919359564d443d51bae25f1deddda77e5
Mikel <aniamarinm@gmail.com>
2026-07-17 21:09:18 -0400

    1.0.3 matriz numerica creada

--- .venv/bin/activate.csh
+++ .venv/bin/activate.csh
@@ -9,7 +9,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV /home/cross/VSCode/Python/Python-Calculadora/.venv
+setenv VIRTUAL_ENV /home/cross/inicio/VSCode/Python/Python-Calculadora/.venv
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/"bin":$PATH"
--- config/gui.py
+++ config/gui.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 
 class Window(QWidget):
 
@@ -10,6 +10,34 @@ class Window(QWidget):
 
         self.start_gui()
 
+    def create_button(self, text, value):
+        button = QPushButton(text)
+        button.value = value
+        button.clicked.connect(lambda: print(f"Soy el boton con el valor: {button.value}"))
+        return button
+
+    def create_row(self, row):
+        button_row = QHBoxLayout()
+        for element in row:
+            button = self.create_button(text = element, value = element)
+            button_row.addWidget(button)
+        return button_row
+
+    def create_matrix(self):
+
+        calculator = QVBoxLayout()
+        elements = [["7", "8", "9", "/"],
+                    ["4", "5", "6", "*"],
+                    ["1", "2", "3", "-"],
+                    ["0", ".", "=", "+"]]
+        
+        for row in elements:
+            button_row = self.create_row(row)
+            calculator.addLayout(button_row)
+        return calculator
+
     def start_gui(self):
 
+        self.calculator = self.create_matrix()
+        self.setLayout(self.calculator)
         self.show()
